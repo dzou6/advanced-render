c85e0c64b4ace019ad588e41a9e53ec5ad83eae4
Zeli <ak47m40080@gmail.com>
2018-11-23 15:32:28 +1100

    prac1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const languages = [
+      {name:'Javascript',rank:1},
+      {name:'Java',rank:2},
+      {name:'Python',rank:3},
+      {name:'Ruby',rank:4},
+      {name:'PHP',rank:5},
+      {name:'C++',rank:6},
+      {name:'CSS',rank:7},
+      {name:'C#',rank:8},
+      {name:'C',rank:9},
+      {name:'Go',rank:10},
+      {name:'Shell',rank:11},
+      {name:'Objective C',rank:12},
+      {name:'Scala',rank:13},
+      {name:'Swift',rank:14},
+      {name:'Typescript',rank:15}
+    ];
+
+    const languageEles = languages.map(language => 
+      <div>
+        <h3>{language.name}</h3>
+        <p>{language.rank}</p>
+      </div>)
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       {languageEles}
       </div>
     );
   }
